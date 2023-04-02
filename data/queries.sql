e81271130532abeb65ac8dbee2a76a35c74fbb8e
Chris Olwell <chrisolwell@gmail.com>
2023-04-02 17:06:14 -0500

    created a new map_db.csv file that joins the geographical data and the yelp review data.

--- data/queries.sql
+++ data/queries.sql
@@ -24,4 +24,16 @@ CREATE TABLE coordinates (
 
 SELECT * FROM yelp;
 
-DROP TABLE yelp;
+--DROP TABLE yelp;
+
+-- SELECT * 
+-- INTO highly_rated
+-- FROM yelp
+-- WHERE rating 
+
+-- SELECT yelp.restaurant, yelp.rating, yelp.price
+--SELECT yelp.season, yelp.episode, yelp.title, yelp.restaurant, yelp.rating, yelp.price
+SELECT coordinates.season, coordinates.episode, coordinates.title, coordinates.restaurant, coordinates.latitude, coordinates.longitude, yelp.rating, yelp.price, yelp.state
+FROM coordinates
+INNER JOIN yelp
+ON yelp.restaurant = coordinates.restaurant;
